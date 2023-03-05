32ada99c43386016499513b89296d9abc42d3bdb
YaminaKat7 <yamina.katariya@gmail.com>
2023-03-04 23:33:27 -0800

    added Home, Episode Recommender and About Us page displays

--- an_analysis_of_nothing/app.py
+++ an_analysis_of_nothing/app.py
@@ -1,112 +1,256 @@
 import pandas as pd
+import numpy as np
+
 import streamlit as st
+from streamlit_option_menu import option_menu
 import plotly.express as px
-import plotly.graph_objects as go
-import util
 
+from utils import data_manager
+from utils import recommender
 
-st.set_page_config(layout="wide")
-# Intro text and image
-try:
-    # Load dataframes into session state once
-    if "df_imdb" not in st.session_state:
-        st.session_state.df_imdb = pd.read_csv('./data/imdb.csv')
-    if "df_script" not in st.session_state:
-        st.session_state.df_script = pd.read_csv('./data/scripts_emo2.csv')
+st.set_page_config(layout="centered")
 
-    col1_1, col1_2 = st.columns(2)
-    with col1_1:
-        st.markdown(
-            """
-                # An Analysis of Nothing
-                ### Search Functionality
+page_bg = """
+<style>
+[data-testid="stAppViewContainer"] > .main {
+    background-color: black
+}
+[data-testid="stHeader"] {
+    background: rgba(0,0,0,0)
+}
+[data-testid="stSidebar"] {
+    background-color: #3580BB;
+}
+[data-testid="stMarkdownContainer"] {
+    color: white;
+}
+</style>
+"""
 
-                Search episodes by keywords, concepts, or topics with the search bar below. Alternatively, choose a couple of your favorite episodes and we will use our proprietary algorithm to suggest some more episodes to watch.
-            """
-        )
+st.markdown(page_bg, unsafe_allow_html=True)
 
-    # Seinfeld image
-    with col1_2:
-        st.image('./static/images/cast.png')  # use_column_width=True)
-except Exception as e:
-    st.error("There was an issue loading data")
+st.image('./static/images/analysis_of_nothing.png')
 
-# Search by phrase and episode recommendation
-try:
-    col2_1, col2_2 = st.columns(2)
-    with col2_1:
-        search_string = st.text_input(
-            "Search Episodes", placeholder="e.g. Episode with mean soup guy")
-        if search_string:
-            search_results = util.search_dataframe(
-                st.session_state.df_imdb, search_string)
-            st.write(search_results[['Title', 'Description',   'Season',
-                                    'EpisodeNo', 'averageRating', 'Director', 'Writers']])
-        else:
-            st.write(st.session_state.df_imdb.sort_values('averageRating', ascending=False).iloc[:5][[
-                'Title', 'Description',   'Season', 'EpisodeNo', 'averageRating', 'Director', 'Writers']])
-    with col2_2:
-        favorite_episodes = st.multiselect(
-            'Similar Episodes', st.session_state.df_imdb['Title'].unique())
-        # TOOO: change this to a better suggestion algorithm
-        if favorite_episodes:
-            fav_ep = st.session_state.df_imdb[st.session_state.df_imdb['Title'].isin(
-                favorite_episodes)]
-            fav_str = ', '.join(
-                fav_ep['Summaries'].apply(lambda x: ''.join(x)))
-
-            similar_episodes = util.search_dataframe(
-                st.session_state.df_imdb[~st.session_state.df_imdb['Title'].isin(favorite_episodes)], fav_str)
-            st.write(similar_episodes)
-except Exception as e:
-    st.write(e)
-    st.error(
-        "There was an issue processing your selections for searching functionality.")
-
-# Analysis functionality
-try:
-    st.markdown(
-        """
-            ### Analysis Functionality
-            Select episodes and see the breakdown of their sentiments by episode as well as the breakdown for each character's dialogue and their sentiments.
-        """
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Main Menu",
+        options=["Home", "Episode Recommender", "Query Episodes", "About Us"],
+        icons=["house", "tv", 'search', 'people'],
+        menu_icon="cast",
+        default_index=0,
+        styles={
+            "container": {"background-color": "white"}
+        }
     )
-    selected_eps = st.multiselect(
-        'Episode Analysis', st.session_state.df_imdb['Title'].unique())
-    if selected_eps:
-        selected = util.get_occurrences(
-            st.session_state.df_imdb, st.session_state.df_script, selected_eps)
 
-        # TODO: Make this more efficient by extracting out new columns into original csv
-        # TODO: Display weighted sum (i.e. rather than argmax take )
-        selected[['Happy', 'Angry', 'Surprise', 'Sad', 'Fear']
-                 ] = selected.apply(util.extract_emotions, axis=1)
-        selected['Count'] = 1
-        selected['Argmax'] = selected.apply(util.extract_argmax, axis=1)
-        grouped_df = selected[['Happy', 'Angry', 'Surprise',
-                               'Sad', 'Fear', 'SEID']].groupby('SEID').sum()
-        grouped_df = grouped_df.reset_index()
-        # Melt the DataFrame to create a long format
-        melted_df = grouped_df.melt(
-            id_vars='SEID', var_name='Emotion', value_name='sum')
+if selected == "Home":
 
-        fig = px.bar(melted_df, x='sum', y='Emotion',
-                     color='SEID', orientation='h')
+    st.markdown("""<h5 style='text-align: center; color: white;'>
+            Explore your favorite Seinfeld moments, characters, and quotes!
+            <br>Use the Episode Recommender to find an episode that's
+            sure to make you laugh.</h5>""",
+            unsafe_allow_html=True)
 
-        fig.update_layout(
-            xaxis_title="Weighted Total",
-            yaxis_title="Sentiment",
-            showlegend=True
-        )
-        fig2 = px.sunburst(
-            selected, path=['Character', 'Argmax'], values='Count')
+    left_1, middle_1, right_1 = st.columns(3)
+    with middle_1:
+        st.image('./static/images/lead.png')
 
-        col3_1, col3_2 = st.columns(2)
-        with col3_1:
-            st.plotly_chart(fig, use_container_width=True)
+    try:
+        # Load dataframes into session state once
+        if ("df_imdb" not in st.session_state) or ("df_dialog" not in st.session_state):
+            st.session_state.df_imdb, st.session_state.df_dialog = data_manager.load_data()
+    except Exception as e:
+        st.error("There was an issue loading data")
 
-        with col3_2:
-            st.plotly_chart(fig2)
-except Exception as e:
-    st.error(
-        "There was an issue processing your data for analysis. Please choose another episode")
+    dialog_df = pd.DataFrame(st.session_state.df_dialog)
+
+    seasons = st.multiselect(
+        'Select Season(s)',
+        dialog_df["Season"].unique(),
+        default=[1,2,3]
+    )
+
+    st.markdown("""<h5 style='text-align: center; color: white;'><br>
+                Lines Spoken by Character and Season</h5>""",
+                unsafe_allow_html=True)
+
+    selected_seasons = np.in1d(dialog_df["Season"], seasons)
+    season_df = dialog_df[selected_seasons]
+
+    main_four = ["JERRY", "GEORGE", "ELAINE", "KRAMER"]
+    characters = np.in1d(season_df["Character"], main_four )
+    character_df = season_df[characters]
+
+    histogram = px.histogram(
+        data_frame = character_df,
+        x = "Character",
+        template="presentation",
+        color = "Season"
+    )
+    histogram.update_layout(
+        paper_bgcolor="rgba(0,0,0,0)",
+        plot_bgcolor = "rgba(0,0,0,0)",
+        legend_font_color="white",
+        legend_title_font_color="white",
+        newshape_line_color = "white"
+    )
+    histogram.update_xaxes(
+        title_font_color="white",
+        tickfont_color="white"
+    )
+    histogram.update_yaxes(
+        title_font_color="white",
+        tickfont_color="white"
+    )
+
+    st.plotly_chart(histogram)
+
+    one, two, three, four = st.columns(4)
+    with one:
+        st.image('./static/images/jerry.png', width=190)
+    with two:
+        st.image('./static/images/george.png', width=170)
+    with three:
+        st.image('./static/images/kramer.png', width=195)
+    with four:
+        st.image('./static/images/elaine.png', width=210)
+
+    st.markdown("""<h5 style='text-align: center; color: white;'><br>
+                Average Episode Rating</h5>""",
+                unsafe_allow_html=True)
+   
+    imdb_df = pd.DataFrame(st.session_state.df_imdb)
+
+    chosen_seasons = np.in1d(imdb_df["Season"], seasons)
+    imdb_season_df = imdb_df[chosen_seasons]
+
+    line_chart = px.line(
+        data_frame = imdb_season_df,
+        x = "SEID",
+        y = "averageRating",
+        labels = {
+            "averageRating": "Average Rating",
+            "SEID": "Season and Episode"
+        },
+        template = "presentation",
+        color = "Season"
+    )
+    line_chart.update_traces(
+        mode = "markers+lines",
+        hovertemplate=None
+    )
+    line_chart.update_layout(
+        paper_bgcolor="rgba(0,0,0,0)",
+        plot_bgcolor = "rgba(0,0,0,0)",
+        legend_font_color="white",
+        legend_title_font_color="white",
+        newshape_line_color = "white"
+    )
+    line_chart.update_xaxes(
+        title_font_color="white",
+        tickfont_color="white"
+    )
+    line_chart.update_yaxes(
+        title_font_color="white",
+        tickfont_color="white"
+    )
+
+    st.plotly_chart(line_chart)
+
+if selected == "Episode Recommender":
+
+    st.markdown("""<h2 style='text-align: center; color: white;'><br>
+                Episode Recommender<br><br></h2>""",
+                unsafe_allow_html=True)
+
+    st.markdown("""<h6 style='text-align: left; color: white;'><br>
+                Please answer the follow prompts for
+                episode recommendations:<br><br></h6>""",
+                unsafe_allow_html=True)
+
+    num_ep = st.selectbox(
+        "How many episodes do you want recommended to you?",
+        options=[1,2,3,4,5],
+        index=0
+    )
+
+    imdb_df_rec = pd.DataFrame(st.session_state.df_imdb)
+    scripts_df_rec = pd.DataFrame(st.session_state.df_dialog)
+
+    title = st.multiselect(
+        label="Enter the title of episodes you've enjoyed.",
+        options=imdb_df_rec["Title"].unique()
+    )
+
+    st.markdown("""<body><p><small><i>
+                *If you don't know the title of your
+                favorite episode, navigate to the Episode
+                Query page to search for the title*
+                </i></small></p></body>""",
+                unsafe_allow_html=True)
+    
+    left_4, middle_4, right_4 = st.columns(3)
+    with middle_4:
+        st.image('./static/images/giphy.gif')
+    
+    st.markdown("""<h6 style='text-align: left; color: white;'><br>
+                Check box to enter and uncheck to reset:</h6>""",
+                unsafe_allow_html=True)
+
+    ready = st.checkbox(
+        label="Ready!"
+    )
+    
+    if ready:
+        with st.spinner("Loading your episodes..."):
+            recs = recommender.Recommender(imdb_df_rec,scripts_df_rec)
+            ranked_ep = recs.find_closest_episodes(n=num_ep, title_list=title)
+            st.dataframe(ranked_ep[["Title", "Season", "EpisodeNo"]], use_container_width=True)
+if selected == "Query Episodes":
+    st.title("hey girl")
+if selected == "About Us":
+
+    st.markdown("""<h2 style='text-align: center; color: white;'><br>
+                About Us<br><br></h2>""",
+                unsafe_allow_html=True)
+    
+    st.markdown("""<h6 style='text-align: left; color: white;'><br>
+                For DATA515 Software Design for Data Scientists
+                course at the University of Washington, we created
+                web application for Seinfeld fans to explore
+                and query dialogue snippets, identify correlations between
+                character appearances and audience ratings, and chronicle
+                appearances of various characters across the series.</h6>""",
+                unsafe_allow_html=True)
+   
+    st.markdown("""<h6 style='text-align: left; color: white;'><br>
+                Creators:</h6>""",
+                unsafe_allow_html=True)
+   
+    left_2, right_2 = st.columns(2)
+    with left_2:
+        st.image('./static/images/kramer_.jpg', width=314)
+        st.markdown("""<h6 style='text-align: center; color: white;'>
+                Yash Manne<br>
+                Project Manager</h6>""",
+                unsafe_allow_html=True)
+    with right_2:
+        st.image('./static/images/elaine_.jpg', width=345)
+        st.markdown("""<h6 style='text-align: center; color: white;'>
+                Aditi Shrivastava<br>
+                Developer<br><br></h6>""",
+                unsafe_allow_html=True)
+       
+    left_3, right_3 = st.columns(2)
+    with left_3:
+        st.image('./static/images/newman_.jpg', width=314)
+        st.markdown("""<h6 style='text-align: center; color: white;'>
+                Yamina Katariya<br>
+                UX Designer<br><br></h6>""",
+                unsafe_allow_html=True)
+    with right_3:
+        st.image('./static/images/george_.jpg', width=345)
+        st.markdown("""<h6 style='text-align: center; color: white;'>
+                Chandler Audlt<br>
+                Quality Assurance<br><br></h6>""",
+                unsafe_allow_html=True)
