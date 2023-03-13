12543acf1bfd719f71ebc2c92312dae1ab7b588a
Yash Manne <manneyas@msu.edu>
2023-03-13 11:18:14 -0700

    changed to run query vector precomputing in scripts/

--- scripts/get_final_data.py
+++ scripts/get_final_data.py
@@ -4,18 +4,17 @@ Prerequisite: Activate conda environment with:
     conda activate nothing
 """
 import os
+import numpy as np
 import pandas as pd
-import text2emotion as te
-from tqdm.auto import tqdm
 
 from data_tools import load_data
-from sentiment_tools import sentiment
+from precompute_tools import sentiment, query_vectors
 
 if __name__ == "__main__":
     cwd = os.getcwd()
     base_path = cwd.split("an_analysis_of_nothing", maxsplit=1)[0]
     data_folder = base_path + \
-        'an_analysis_of_nothing/an_analysis_of_nothing/data/'
+        'an_analysis_of_nothing/an_analysis_of_nothing/static/data/'
     if not os.path.exists(data_folder):
         os.mkdirs(data_folder)
     meta_name = data_folder + 'metadata.csv'
@@ -27,4 +26,8 @@ if __name__ == "__main__":
     print("Done gathering emotions.")
     meta.to_csv(meta_name, index=False)
     scripts.to_csv(script_name, index=False)
-    print('Done saving data to an_analysis_of_nothing/data/')
+    print(f'Done saving data to {data_folder}')
+    print("Now precomputing search query vectors...")
+    query_vectors.create_corpus_embeddings(df_script=scripts)
+    print("Done saving precomputed search query vectors.")
+
