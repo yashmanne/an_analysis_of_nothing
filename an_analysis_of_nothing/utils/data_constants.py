d05fb1456ca2e70c3799df415d8414e38560fdd3
Yash Manne <manneyas@msu.edu>
2023-03-13 09:36:23 -0700

    updated data to be saved locally under static/data

--- an_analysis_of_nothing/utils/data_constants.py
+++ an_analysis_of_nothing/utils/data_constants.py
@@ -18,7 +18,7 @@ Information:
     * **Sad**: Overall sadness sentiment of line.
     * **numWords**: number of whitespace-separated words in dialogue.
 
-* `meta.csv`: Data providing additional metadata for each episode.
+* `metadata.csv`: Data providing additional metadata for each episode.
     * **Season**: Season number.
     * **EpisodeNo**: Episode number (According to Wikipedia).
     * **AirDate**: Date of original airing of episode.
@@ -40,9 +40,11 @@ GDRIVE_BASE = 'https://drive.google.com/uc?id='
 
 # Scripts
 # pylint: disable=line-too-long
-SCRIPTS_LINK = 'https://drive.google.com/file/d/1zd58WSVxmebSMOMY9zM8myOHqMcKyAX9/view?usp=sharing'
-SCRIPTS_LINK = GDRIVE_BASE + SCRIPTS_LINK.split('/')[-2]
+# SCRIPTS_LINK = 'https://drive.google.com/file/d/1zd58WSVxmebSMOMY9zM8myOHqMcKyAX9/view?usp=sharing'
+# SCRIPTS_LINK = GDRIVE_BASE + SCRIPTS_LINK.split('/')[-2]
+SCRIPTS_LINK = './static/data/scripts.csv'
 # Episode Info
 # pylint: disable=line-too-long
-EPISODE_LINK = 'https://drive.google.com/file/d/1VA6wa3lc9LnmJSe8I8EtP82Ooc4SQfwz/view?usp=sharing'
-EPISODE_LINK = GDRIVE_BASE + EPISODE_LINK.split('/')[-2]
+# EPISODE_LINK = 'https://drive.google.com/file/d/1VA6wa3lc9LnmJSe8I8EtP82Ooc4SQfwz/view?usp=sharing'
+# EPISODE_LINK = GDRIVE_BASE + EPISODE_LINK.split('/')[-2]
+EPISODE_LINK = './static/data/metadata.csv'
