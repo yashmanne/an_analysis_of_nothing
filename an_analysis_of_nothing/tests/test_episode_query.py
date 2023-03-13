18ce6b814ddf62d7ef676fc89714c842868eb34c
DreamWeaver2k <chandlerault@gmail.com>
2023-03-12 19:38:19 -0700

    added data constants check

--- an_analysis_of_nothing/tests/test_episode_query.py
+++ an_analysis_of_nothing/tests/test_episode_query.py
@@ -1,9 +1,22 @@
 import unittest
 from unittest.mock import patch
 import pandas as pd
+import numpy as np
 import torch
-from utils import data_manager, episode_query
+from utils import data_manager, episode_query, data_constants
 
+class MockObject:
+    def __init__(self):
+        pass
+    def __getattr__(self, name):
+        if name == 'query':
+            return load_corpus()
+        else:
+            raise AttributeError(f"'MockObject' object has no attribute '{name}'")
+
+def load_corpus():
+    imdb, script = data_manager.load_data()
+    return episode_query.load_corpus(imdb, script)
 
 def mocked_read_csv(*args):
     """
@@ -105,6 +118,90 @@ def mocked_read_csv(*args):
     if args[0] == 'https://drive.google.com/uc?id=1VA6wa3lc9LnmJSe8I8EtP82Ooc4SQfwz':
         return mock_imdb_data
     elif args[0] == 'https://drive.google.com/uc?id=1zd58WSVxmebSMOMY9zM8myOHqMcKyAX9':
+        return mock_script_data
+    else:
+        return mock_imdb_data
+
+def mocked_read_csv_large(*args):
+    """
+        This is a mock function for loading data.
+    """
+    data_imdb_raw = [
+        [1, 1, "July 5, 1989", "Larry David, Jerry Seinfeld", "Art Wolff", "S01E01", "tt0098286",
+        "Good News, Bad News", 1, 23, "6031", 7.4, "Jerry and George argue \
+        whether an overnight visitor \
+        Jerry is expecting is coming with romantic intentions. ", """["In \
+        this episode, the predecessor to Seinfeld \
+        (1989), Jerry is expecting a woman that he met in Michigan to come and visit him in New York.\
+        Throughout the first part of the show Jerry and \
+        George are discussing the situation. Later we meet 'Kessler' \
+        who comes in to Jerry\'s apartment to borrow some \
+        meat and uncharacteristically \
+        knocks on the door before entering."]""", "['tv series pilot', \
+        'cafe', 'waitress', 'coffee', 'friend', 'apartment', 'airport',\
+        'greeting', 'wine', 'dog', 'bread', 'meat', 'mattress', \
+        'laundromat', 'romantic interest', 'realtor', 'overnight', \
+        'visitor', 'intentions', 'milk', 'decaf', 'purple shirt', 'button', \
+        'purple', 'shirt', 'misunderstanding', 'watching tv', 'clothes dryer',\
+        'refrigerator', \
+        'sandwich', 'sitting on a couch', 'eating cereal', \
+        'cereal', 'magazine', 'waiting', 'surprise', 'handshake', \
+        'luggage', 'help', 'relaxation', 'hospitality', 'lap', 'man takes off shoes', \
+        'wine bottle', 'red wine', 'evening', 'audience', 'watching sports on tv']"],
+
+        [1, 2, "May 31, 1990", "Larry David, Jerry Seinfeld", "Tom Cherones", "S01E02", "tt0697784",
+        "The Stakeout", 2, 23, "5113", 9, "Jerry and George stake \
+        out the lobby of an office building to \
+        find a woman Jerry met at a party but whose name and phone \
+        number he didn't get.",
+        """["Elaine drags Jerry to a birthday dinner for one of her friends \
+        where he meets an attractive woman. \
+        He promptly forgets her name and refuses to ask Elaine who she is but \
+        remembers where she works. Jerry's parents are \
+        in town to go to a family wedding and his Dad suggests he stake-out \
+        the lobby around lunchtime. George tags along \
+        but they can't quite seem to get their stories straight. Elaine hears \
+        about it making them both uncomfortable."]""",
+        "['reference to loni anderson', 'pornography', 'fake name', \
+        'scrabble', 'hearing characters thoughts', 'made up word', \
+        'reference to john cougar mellencamp', 'reference to ethel merman', \
+        'polishing shoes', 'birthday party', 'dream', 'video store']"],
+
+        [2, 2, "May 31, 1990", "Larry David, Jerry Seinfeld", "Tom Cherones", 
+        "S02E02", "tt0697784",
+        "The Stakeout", 2, 23, "5113", 1, "Jerry and George stake \
+        out the lobby of an office building to \
+        find a woman Jerry met at a party but whose name and phone \
+        number he didn't get.",
+        """["Elaine drags Jerry to a birthday dinner for one of her friends \
+        where he meets an attractive woman. \
+        He promptly forgets her name and refuses to ask Elaine who she is but \
+        remembers where she works. Jerry's parents are \
+        in town to go to a family wedding and his Dad suggests he stake-out \
+        the lobby around lunchtime. George tags along \
+        but they can't quite seem to get their stories straight. Elaine hears \
+        about it making them both uncomfortable."]""",
+        "['reference to loni anderson', 'pornography', 'fake name', \
+        'scrabble', 'hearing characters thoughts', 'made up word', \
+        'reference to john cougar mellencamp', 'reference to ethel merman', \
+        'polishing shoes', 'birthday party', 'dream', 'video store']"],
+    ]
+    data_scripts_raw = [
+        ['GEORGE', 'Are you through?', 'S01E01', 1, 1, 0, 0, 0, 0, 0, 3],
+    ] * 26561
+
+
+    mock_imdb_data = pd.DataFrame(data_imdb_raw, columns=['Season',
+        'EpisodeNo', 'AirDate', 'Writers', 'Director', 'SEID', 'tconst',
+        'Title', 'EpiNo_Netflix', 'runtimeMinutes', 'numVotes', 'averageRating',
+        'Description', 'Summaries', 'keyWords'])
+    mock_script_data = pd.DataFrame(data_scripts_raw, columns=[
+        'Character', 'Dialogue', 'SEID', 'Season', 'EpisodeNo',
+        'Happy', 'Angry', 'Surprise', 'Sad', 'Fear', 'numWords'])
+
+    if args[0] == data_constants.SCRIPTS_LINK:
+        return mock_imdb_data
+    elif args[0] == data_constants.EPISODE_LINK:
         return mock_script_data
     else:
         return mock_imdb_data
@@ -406,11 +503,34 @@ class TestLoadCorpus(unittest.TestCase):
         Smoke test for load corpus
         """
         imdb, script = data_manager.load_data()
-        c, c_emb, emb = episode_query.load_corpus(imdb, script)
+        c, c_emb, _ = episode_query.load_corpus(imdb, script)
         
         self.assertIsInstance(c, object)
         self.assertIsInstance(c_emb, torch.Tensor)
 
+class TestFilterSearchResults(unittest.TestCase):
+    """
+    Test class for the filter_search_results() method
+    """
+    def setUp(self):
+        pass
 
-
-
+    # @patch('utils.data_manager.pd.read_csv', side_effect=mocked_read_csv)
+    @patch('streamlit.session_state', MockObject())
+    def test_all_values(self):
+        """
+        Tests when non of arguments are null
+        """
+        imdb, script = data_manager.load_data()
+        search_string = "Jerry waits in lobby"
+        season_choice = [4]
+        rating_choice = [7,10]
+        char_choice = ['JERRY']
+        filtered_df, search_results = episode_query.filter_search_results(search_string, season_choice, rating_choice,
+                          char_choice, imdb, script)
+        print(filtered_df.head())
+        print(search_results.head())
+        self.assertEqual(True, filtered_df.iloc[0]['char_check'])
+        self.assertEqual(4, filtered_df.iloc[0]['Season'])
+        self.assertGreater(np.min(filtered_df['averageRating']), 6.9)
+        
